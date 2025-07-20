969794b75dbb9eed14a1eb14217ea0cdea5f727d
Parthkuamr Rathod <parthrathodtk@gmail.com>
2025-07-20 11:57:38 +0530

    Frame Rate Independt movement

--- Header/Gameplay/GameplayManager.h
+++ Header/Gameplay/GameplayManager.h
@@ -2,8 +2,10 @@
 #include "../../Header/Gameplay/Boundary/Boundary.h"
 #include "../../Header/Gameplay/Ball/Ball.h"
 #include "../../Header/Gameplay/Paddle/Paddle.h"
+#include "../../Header/Utility/TimeService.h"
 #include "../../Header/Event/EventManager.h"
 using namespace Event;
+using namespace Utility;
 
 namespace Gameplay
 {
@@ -17,6 +19,7 @@ namespace Gameplay
 		float player2_postion_y = 300.0f;
 
 		EventManager* event_manager;
+		TimeService* time_service = new TimeService();
 		Boundary* boundary = new Boundary();
 		Ball* ball = new Ball();
 		Paddle* player1 = new Paddle(player1_position_x, player1_position_y);
@@ -25,9 +28,7 @@ namespace Gameplay
 	public:
 		GameplayManager(EventManager* manager);
 
-		void CheckRoundState();
-
-		void resetPlayers();
+		void resetPlayer();
 
 		void update();
 		void render(RenderWindow* game_window);
--- Source/Gameplay/GameplayManager.cpp
+++ Source/Gameplay/GameplayManager.cpp
@@ -5,14 +5,22 @@ namespace Gameplay
 {
 	GameplayManager::GameplayManager(EventManager* manager)
 	{
+		time_service->initialize();
 		event_manager = manager;
+	}
+
+	void GameplayManager::resetPlayer()
+	{
+		player1->reset(player1_position_x, player1_position_y);
+		player2->reset(player2_postion_x, player2_postion_y);
 	}
 
 	void GameplayManager::update()
 	{
-		player1->update(event_manager->isKeyPressed(Keyboard::W), event_manager->isKeyPressed(Keyboard::S));
-		player2->update(event_manager->isKeyPressed(Keyboard::Up), event_manager->isKeyPressed(Keyboard::Down));
-		ball->update(player1, player2);
+		time_service->update();
+		player1->update(event_manager->isKeyPressed(Keyboard::W), event_manager->isKeyPressed(Keyboard::S), time_service);
+		player2->update(event_manager->isKeyPressed(Keyboard::Up), event_manager->isKeyPressed(Keyboard::Down), time_service);
+		ball->update(player1, player2, time_service);
 	}
 
 	void GameplayManager::render(RenderWindow* game_window)
