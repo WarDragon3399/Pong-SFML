70854edae7b993476164f000f699ec2877a1e07b
Parthkuamr Rathod <parthrathodtk@gmail.com>
2025-07-20 11:31:53 +0530

    Boundary

--- Header/Gameplay/GameplayManager.h
+++ Header/Gameplay/GameplayManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../../Header/Gameplay/Boundary/Boundary.h"
 #include "../../Header/Gameplay/Ball/Ball.h"
 #include "../../Header/Gameplay/Paddle/Paddle.h"
 #include "../../Header/Event/EventManager.h"
@@ -13,17 +14,21 @@ namespace Gameplay
 		float player2_postion_x = 1210.0f;
 		float player2_postion_y = 300.0f;
 
-		Ball* ball;
-		Paddle* player1;
-		Paddle* player2;
 		EventManager* event_manager;
-
-
+		Boundary* boundary = new Boundary();
+		Ball* ball = new Ball();
+		Paddle* player1 = new Paddle(player1_position_x, player1_position_y);
+		Paddle* player2 = new Paddle(player2_postion_x, player2_postion_y);
 
 		void initialize();
 
 	public:
 		GameplayManager(EventManager* manager);
+
+		void CheckRoundState();
+
+		void resetPlayers();
+
 		void update();
 		void render(RenderWindow* game_window);
 	};
--- Source/Gameplay/GameplayManager.cpp
+++ Source/Gameplay/GameplayManager.cpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "../../Header/Gameplay/GameplayManager.h"
-#include "../../Header/Event/EventManager.h"
+
+
+
 using namespace Event;
 namespace Gameplay
 {
@@ -9,22 +11,15 @@ namespace Gameplay
 		event_manager = manager;
 	}
 
-	void GameplayManager::initialize() {
-		ball = new Ball();
-		player1 = new Paddle(player1_position_x, player1_position_y);
-		player2 = new Paddle(player2_postion_x, player2_postion_y);
-	}
-
 	void GameplayManager::update()
 	{
-		//previous code
-		player1->update(event_manager->isKeyPressed(Keyboard::W),
-			event_manager->isKeyPressed(Keyboard::S));
-		player2->update(event_manager->isKeyPressed(Keyboard::Up),
-			event_manager->isKeyPressed(Keyboard::Down));
+		player1->update(event_manager->isKeyPressed(Keyboard::W), event_manager->isKeyPressed(Keyboard::S));
+		player2->update(event_manager->isKeyPressed(Keyboard::Up), event_manager->isKeyPressed(Keyboard::Down));
+		ball->update(player1, player2);
 	}
 	void GameplayManager::render(RenderWindow* game_window)
 	{
+		boundary->render(game_window);
 		ball->render(game_window);
 		player1->render(game_window);
 		player2->render(game_window);
